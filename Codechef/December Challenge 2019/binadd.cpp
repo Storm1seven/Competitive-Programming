a59d28f5ec659e2cc2b8af2082ba4e13ee9aa76a
Subhash Reddy <42376739+Storm1seven@users.noreply.github.com>
2019-12-09 22:03:53 +0530

    added solution

--- Codechef/December Challenge 2019/binadd.cpp
+++ Codechef/December Challenge 2019/binadd.cpp
@@ -1,65 +1,95 @@
-# include<bits/stdc++.h>
-# include<unordered_map>
-# include<unordered_set>
+#include <bits/stdc++.h>
 using namespace std;
-# define ll long long
-// # define ll int
-# define vll vector<ll>
-# define vvll vector<vector<ll> >
-# define vchar vector<char>
-# define vstr vector<string>
-# define vpll vector<pair<ll, ll> >
-# define mll map<ll, ll>
-# define sll set<ll>
-# define usll unordered_set<ll>
-# define schar set<char>
-# define pll pair<ll, ll>
-# define append_left push_front
-# define append push_bac
-# define pop_left pop_front
-# define popb pop_back
-# define add insert
-# define all(v) v.begin(), v.end()
-# define rall(v) v.rbegin(), v.rend()
-# define loop(i, k, n, inc) for(ll i = k; i < n; i+=inc)
-# define rloop(i, k, n, inc) for(ll i = k; i > n; i+=inc)
-vll vin(ll n){vll a(n);loop(i, 0, n, 1) cin>>a[i];return a;}
-ll intin() {ll n; cin>>n; return n;}
-char charin(){char a; cin>>a; return a;}
-string strin(){string s; cin>>s; return s;}
-ll factorial(ll n){return (n==1 || n==0) ? 1: n * factorial(n - 1);}
+#define 	pb 				push_back
+#define 	MOD 			1000000007
+#define 	pp 				pop_back
+#define 	MAXN   			1000001
+#define 	mp 				make_pair
+#define 	endl 			'\n'
+#define 	ff 				first
+#define 	ss 				second
+#define 	mapcl 			map<char,ll>
+#define 	mapll 			map<ll,ll>
+#define 	pll 			pair<ll,ll>
+#define 	cmp 			complex<double>
+#define 	pi 				3.141592653589793238462643383279502884197169399375105820974944592307816406286 
+#define 	inf 			1000
+#define 	flush 			fflush(stdout)
+#define 	vll 			vector<ll>
+#define 	all(v) 			v.begin(),v.end()
+#define 	fr(i,z,n) 		for(ll i=z;i<n;i++)
+#define 	sqrt 			sqrtl
+#define 	cbrt 			cbrtl
+#define		ll 				long long
+#define 	ld 				long double
+bool 		coll(pll p1,pll p2,pll p3)		{if((p3.ss-p2.ss)*(p2.ff-p1.ff)==(p2.ss-p1.ss)*(p3.ff-p2.ff))return true;else return false;}
+void		print(auto x)					{cout<<x<<endl;}
+void 		pv(vector<auto> v)				{for(ll i=0;i<(ll)v.size();i++)cout<<v[i]<<" ";cout<<endl;}
+void 		pvsl(vector<auto> v)			{for(ll i=0;i<(ll)v.size();i++)cout<<v[i]<<" ";}
+void 		in(vector<auto> &v, ll n)		{v.resize(n);for(ll i=0;i<n;i++)cin>>v[i];}
+void 		sortv(vector<auto> &v)			{sort(v.begin(),v.end());}
+void 		fast()							{ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+ll maxi(ll a, ll b){ 
+	if (a > b)
+		return a;
+	return b;
+}
+ll mini(ll a, ll b){
+	if (a > b)
+		return b;
+	return a;
+}
+
+bool check(string a){
+	for (ll i=0;i<(ll)a.size();i++){
+		if (a[i] == '1'){
+			return false;
+		}
+	}
+	return true;
+}
+
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    ll t; cin>>t;
-    loop(i, 0, t, 1){
-        string a, b;
-        cin>>a>>b;
-        if (a == "0"){
+	ll t;
+	cin >> t;
+	while (t--){
+		string a, b;
+		cin >> a >> b;
+		if (check(b)){
+			cout << 0 << endl;
+			continue;
+		}
+        else if(check(a)){
             cout<<1<<endl;
+            continue;
         }
-        else if (b == "0"){
-            cout<<0<<endl;
-        }
-        else{
-            cout<<"YES"<<endl;
-            ll ai = a.size()-1, bi = b.size()-1, ci = 0;
-            ll count = 0;
-            while (ai > 0 and bi > 0){
-                cout<<"HI"<<endl;
-                if (a[ai]-'0' + b[bi]-'0' + ci >= 2){
-                    ci=1;
-                    if (a[ai]-'0' + b[bi]-'0' + ci == 2){
-                        count++;
-                    }
-                }
-                else{
-                    ci = 0;
-                }
-                ai-=1;
-                bi-=1;
-            }
-            cout<<count+ci<<endl;
-        }
-    }
-    return 0;
+		reverse(a.begin(), a.end());
+		reverse(b.begin(), b.end());
+		while (a.size() != b.size()){
+			if (a.size() > b.size()){
+				b += '0';
+			}
+			else{
+				a += '0';
+			}
+		}
+		a += '0';
+		b += '0';
+		reverse(a.begin(), a.end());
+		reverse(b.begin(), b.end());
+		ll lzero = 0;
+		ll ans = -1;
+		for (ll i=1;i<(ll)b.size();i++){
+			if (b[i] == '1' && a[i] == '1'){
+				ans = maxi(ans, i - lzero + 1);
+				lzero = i;
+			}
+			else if (a[i] == '0' && b[i] == '0'){
+				lzero = i;
+			}
+		}
+        if (ans == -1) cout<<1<<endl;
+        else cout<<ans<<endl;
+	}
+	return 0;
 }
